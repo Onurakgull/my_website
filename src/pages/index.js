99176e73970dc94536bc93bf6c4dc8acc3d00c55
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-06 21:00:00 +0300

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Name from '../components/Name'
+import Content from '../components/Content'
+
+const Home = () => {
+    return (
+        <div>
+            <Name/>
+            <Content/>
+        </div>
+    )
+}
+
+export default Home;

7fe984b4d085f29735fe60a3879ebdeaa2873464
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-07 00:11:08 +0300

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Menu = () => {
     return (
-        <div>
-            
+        <div className="h-screen flex justify-center items-center bg-gray-300 ">
+                <h1 className="text-9xl uppercase font-black">Menu</h1>
         </div>
     )
 }

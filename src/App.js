55387b90f5bfe335119f5d3ea5a7cfd2f2f16196
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2021-12-07 00:10:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React, {useState, useEffect} from 'react';
 import Navbar from './components/Navbar';
-import Name from './components/Name'
+import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
+import Home from './pages';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Dropdown from './components/Dropdown';
+// import Contact from './pages/Contact';
+
 
 function App() {
+  const [isOpen, setIsOpen]=useState(false);
+
+  const toggle=()=>{
+    setIsOpen(!isOpen)
+  }
+  useEffect(()=>{
+    const hideMenu=()=>{
+      if(window.innerWidth>768 && isOpen){
+        setIsOpen(false);
+        console.log("Bit amk");
+      }
+    };
+    window.addEventListener('resize', hideMenu)
+
+    return()=>{
+      window.removeEventListener('resize', hideMenu)
+    }
+  })
   return (
     <>
-      <Navbar/>
-      <Name/>
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
+      <Routes>
+        <Route path="/" caseSensitive={false} element={<Home/>} />
+        <Route path='/About' caseSensitive={false} element={<About/>} />
+        <Route path='/Menu' caseSensitive={false} element={<Menu/>} />
+        {/* <Route path='/Contact' caseSensitive={false} element={<Contact/>} /> */}
+        
+      </Routes>
+      <Footer/>
     </>
   );
 }
